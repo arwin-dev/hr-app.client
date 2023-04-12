afac63771f609178355fba6f464e6e69d0908ecc
Arwin <63482587+arwin-dev@users.noreply.github.com>
2023-04-11 20:36:33 -0400

    navbar added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import {Login} from './Components/Login';
 import EmployeeList from './Components/EmployeeList'
 import { AuthProvider } from './Components/auth';
 import RequireAuth from './Components/RequireAuth';
+import Navbar from './Components/Navbar';
+import NoMatch from './Components/NoMatch';
 
 function App() {
   return (
+    
     <AuthProvider>
-      <Routes>
-        <Route path='login' element={< Login />}/>
-        <Route path='dashboard' element={ <RequireAuth>< EmployeeList /></RequireAuth> }/>
-      </Routes>
+      <>
+        <Navbar/>
+        <Routes>
+          <Route path='login' element={< Login />}/>
+          <Route path='dashboard' element={ <RequireAuth>< EmployeeList /></RequireAuth> }/>
+          <Route path='*' element={ <NoMatch/> }/>
+        </Routes>
+      </>
     </AuthProvider>
   );
 }
